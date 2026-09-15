3e9e5e57a1421e57486690a1ee98d3a68f5ac1e4
Md Imam Hussain <mihussain737@gmail.com>
2026-09-15 08:05:34 +0530

    eureka server project added and eureka client added for all services

--- metering-service/src/main/java/com/cis/metering_service/client/ConsumerClient.java
+++ metering-service/src/main/java/com/cis/metering_service/client/ConsumerClient.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "new-connection-service" ,url = "http://localhost:8082",configuration = FeignClientConfig.class)
+@FeignClient(name = "new-connection-service",
+        configuration = FeignClientConfig.class)
 public interface ConsumerClient {
 
     @GetMapping("/api/consumers/{consumerId}")
